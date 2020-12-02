de39b956433bda7da7b4ca61f37bf2699bf542da
Julien Kluge <Julien.Kluge@gmail.com>
2020-12-02 18:11:08 +0100

    Implemented Basic inheritance and USB working

--- src/TEC.py
+++ src/TEC.py
@@ -6,39 +6,48 @@ class MeerstetterTEC:
     It can be put in any interface needed and is used by the implemented interfaces in this package.
     """
 
-    def __init__(self, address = 0):
+    def __init__(self, tec_address = 0, check_crc = True):
 
         #address
-        if (type(address) == str):
+        if (type(tec_address) == str):
             try:
-                address = int(address)
+                tec_address = int(tec_address)
             except:
                 raise Exception("Could not convert address str into an int")
-        if (type(address) == int):
-            if (address < 0 or address > 255):
+        if (type(tec_address) == int):
+            if (tec_address < 0 or tec_address > 255):
                 raise Exception("Address argument is either below 0 or above 255")
-            self.address = "{:02x}".format(address)
+            self.tec_address = "{:02x}".format(tec_address)
         else:
             raise Exception("Type of address is not int or str")
 
         #sequence number, is increased whenever a new request is made
-        self.sequence_number = 5545
+        self.sequence_number = -1
+        self.check_crc = check_crc
+    
+    def _formIdentificationCommand(self):
+        self._advanceSequenceNumber()
+        command = "#{}{:04X}?IF".format(self.tec_address, self.sequence_number)
+        return self._appendCRC(command.encode())
 
-    def formSetCommand(self, number, channel, value):
+    def _formSetCommand(self, number, channel, value):
         self._advanceSequenceNumber()
         command = "#{}{:04X}"
 
-    def formReadCommand(self, number, channel):
+    def _formReadCommand(self, number, channel):
         self._advanceSequenceNumber()
-        command = "#{}{:04X}?VR{:04X}{:02X}".format(self.address, self.sequence_number, number, channel)
+        command = "#{}{:04X}?VR{:04X}{:02X}".format(self.tec_address, self.sequence_number, number, channel)
         return self._appendCRC(command.encode())
 
     def _appendCRC(self, command_str):
+        return command_str + self.form_crc(command_str)
+
+    def form_crc(self, command_str):
         crc = 0
         for b in command_str:
             crc = self._crcRound(crc, int(b))
         crc_str = "{:04X}".format(crc)
-        return command_str + crc_str.encode()
+        return crc_str.encode()
 
     def _crcRound(self, crc, byt):
         genPoly = 0x1021
@@ -56,3 +65,48 @@ class MeerstetterTEC:
         self.sequence_number = self.sequence_number + 1
         if (self.sequence_number > 65535):
             self.sequence_number = 0
+    
+    def _validate_answer(self, answer):
+        if (not self.check_crc):
+            return True
+        body_arr = answer[:-4]
+        test_crc = answer[-4:]
+        calc_crc = self.form_crc(body_arr)
+        if (test_crc != calc_crc):
+            raise Exception("Mismatch in checksums.")
+        return True
+    
+    def _extract_payload(self, answer):
+        return answer[7:-4]
+    
+
+    #
+    #
+    # public functions
+    #
+    #
+    
+
+    def get_id(self):
+        command = self._formIdentificationCommand()
+        answer = self._sendAndReceive(command)
+        self._validate_answer(answer)
+        id_str = self._extract_payload(answer)
+        return id_str.decode()
+    
+    def get_value(self):
+        raise NotImplementedError()
+    def set_value(self):
+        raise NotImplementedError()
+    
+
+    #
+    #
+    # inheritance functions
+    #
+    #
+
+
+    def _sendAndReceive(self, byteArr):
+        raise NotImplementedError()
+    
--- src/TEC_USB.py
+++ src/TEC_USB.py
@@ -1,9 +1,49 @@
 from serial import Serial
 from TEC import MeerstetterTEC
 
-class MeerstetterTEC_USB:
+class MeerstetterTEC_USB(MeerstetterTEC):
     """
     Discrete implementation of the MeerstetterTEC class on an USB based interface
     """
-    def __init__(self, port = "", timeout = 1, baudrate = 57600):
-        pass
+    def __init__(self, port, timeout = 1, baudrate = 57600, tec_address = 0, check_crc = True):
+        super().__init__(
+            tec_address = tec_address,
+            check_crc = check_crc
+        )
+
+        self.port = port
+        self.timeout = timeout
+        self.baudrate = baudrate
+
+        self.ser = Serial(
+            port = self.port,
+            timeout = self.timeout,
+            baudrate = self.baudrate,
+        )
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.ser.__exit__(exc_type, exc_val, exc_tb)
+
+    def __enter__(self):
+        return self
+
+    def stop(self):
+        self.ser.flush()
+        self.ser.close()
+    
+    def _sendAndReceive(self, frame):
+        byte_arr = frame + b'\r'
+
+        #clear all remaining buffers
+        self.ser.reset_output_buffer()
+        self.ser.reset_input_buffer()
+        
+        #send and flush
+        self.ser.write(byte_arr)
+        self.ser.flush()
+
+        #read all in
+        answer_arr = self.ser.read_until(terminator = b'\r')
+        return answer_arr[0:-1]
+
+
